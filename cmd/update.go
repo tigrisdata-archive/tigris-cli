95465713bcd415e4bca322b9d253e2e9a92063e0
Ovais Tariq <ot@tigrisdata.com>
2022-04-27 19:35:28 -0700

    update

--- cmd/update.go
+++ cmd/update.go
@@ -32,6 +32,13 @@ Examples:
   # Update the field "name" of user where the value of the id field is 2
   %[1]s update testdb users '{"id": 19}' '{"$set": {"name": "Updated New User"}}'
 `, rootCmd.Root().Name()),
+	Example: fmt.Sprintf(`Updates the field values in documents according to provided filter.
+
+Examples:
+
+  # Update the field "name" of user where the value of the id field is 2
+  %[1]s update testdb users '{"id": 19}' '{"$set": {"name": "Updated New User"}}'
+`, rootCmd.Root().Name()),
 	Args: cobra.MinimumNArgs(4),
 	Run: func(cmd *cobra.Command, args []string) {
 		ctx, cancel := util.GetContext(cmd.Context())
