2afe11bed0fbc96049c0e6420009572322bca613
Yevgeniy Firsov <firsov@tigrisdata.com>
2022-09-12 10:10:00 -0700

    fix: Fix publishing windows binaries

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,20 +2,25 @@
 
 set -ex
 
-LINK_ID=(lnk_1EY9_24JMe7 lnk_1EY9_24JIYa lnk_1EY9_217mHr lnk_1EY9_216bS8 lnk_1EY9_219th2)
-OS=(linux-arm64 darwin-arm64 linux-amd64 darwin-amd64 windows-amd64)
+LINK_ID=(lnk_1EY9_24JMe7 lnk_1EY9_24JIYa lnk_1EY9_217mHr lnk_1EY9_216bS8 lnk_1EY9_219th2 lnk_1EY9_2Z2L8m)
+OS=(linux-arm64 darwin-arm64 linux-amd64 darwin-amd64 windows-amd64 windows-arm64)
 
-for i in $(seq 0 4); do
+for i in $(seq 0 5); do
 	L=${LINK_ID[i]}
 	O=${OS[i]}
 
+	EXT="tar.gz"
+	if [[ "$O" == "windows"* ]]; then
+		EXT="zip"
+	fi
+
 	#Check that link we are about to publish is live
-	curl -f -s -I "https://github.com/tigrisdata/tigris-cli/releases/download/${VERSION}/tigris-${VERSION}-$O.tar.gz"
+	curl -f -s -I "https://github.com/tigrisdata/tigris-cli/releases/download/${VERSION}/tigris-${VERSION}-$O.$EXT"
 
 	#Push to Shorts.io
 	curl -f -s -X POST \
 		-H "authorization: ${API_KEY}" \
 		-H "content-type: application/json" \
-		-d '{"domain": "tigris.dev", "originalURL": "https://github.com/tigrisdata/tigris-cli/releases/download/'"${VERSION}"'/tigris-'"${VERSION}"'-'"$O"'.tar.gz" }' \
+		-d '{"domain": "tigris.dev", "originalURL": "https://github.com/tigrisdata/tigris-cli/releases/download/'"${VERSION}"'/tigris-'"${VERSION}"'-'"$O"'.'"$EXT"'" }' \
 		https://api.short.io/links/"$L"/
 done
