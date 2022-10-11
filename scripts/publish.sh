9ef2fe146251ad905ca5a84e98460808b5b23562
Yevgeniy Firsov <firsov@tigrisdata.com>
2022-10-10 22:38:38 -0700

    feat: Migrate releases to goreleaser
    * Go releser is very popular and powerful
    * Also we are getting:
        * Linux deb, rpm, apk packaging
        * Automatic build and push to Homebrew tap.
        * Build and push to Snapcraft
        * Builds signing

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -17,7 +17,7 @@
 set -ex
 
 LINK_ID=(lnk_1EY9_24JMe7 lnk_1EY9_24JIYa lnk_1EY9_217mHr lnk_1EY9_216bS8 lnk_1EY9_219th2 lnk_1EY9_2Z2L8m)
-OS=(linux-arm64 darwin-arm64 linux-amd64 darwin-amd64 windows-amd64 windows-arm64)
+OS=(linux_arm64 darwin_arm64 linux_amd64 darwin_amd64 windows_amd64 windows_arm64)
 
 for i in $(seq 0 5); do
 	L=${LINK_ID[i]}
@@ -29,12 +29,12 @@ for i in $(seq 0 5); do
 	fi
 
 	#Check that link we are about to publish is live
-	curl -f -s -I "https://github.com/tigrisdata/tigris-cli/releases/download/${VERSION}/tigris-${VERSION}-$O.$EXT"
+	curl -f -s -I "https://github.com/tigrisdata/tigris-cli/releases/download/${VERSION}/tigris_${VERSION#v}_$O.$EXT"
 
 	#Push to Shorts.io
 	curl -f -s -X POST \
 		-H "authorization: ${API_KEY}" \
 		-H "content-type: application/json" \
-		-d '{"domain": "tigris.dev", "originalURL": "https://github.com/tigrisdata/tigris-cli/releases/download/'"${VERSION}"'/tigris-'"${VERSION}"'-'"$O"'.'"$EXT"'" }' \
+		-d '{"domain": "tigris.dev", "originalURL": "https://github.com/tigrisdata/tigris-cli/releases/download/'"${VERSION}"'/tigris_'"${VERSION#v}"'_'"$O"'.'"$EXT"'" }' \
 		https://api.short.io/links/"$L"/
 done
